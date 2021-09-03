2b94320b55a6bf27c03745a8dd9fb5115d30196f
Gabriel Ferreira <53006243+GabrielFerrato@users.noreply.github.com>
2021-09-03 14:49:24 -0300

    update chatbot

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+#importar bibliotecas necessarias 
 import pywhatkit
 import keyboard
 import time
 from datetime import datetime
-
+# Definir para quais contatos vamos testar
 contatos = [+'5561983545254', '+5561983856137']
- 
+# Intervalo de envio
 while len(contatos) >= 1:
-    pywhatkit.sendwhatmsg(contatos[0], 'oi!', )
+    #enviar mensagens
+    pywhatkit.sendwhatmsg(contatos[0], 'teste!', datetime.now ().hour, datetime.now().minute + 2)
+    del contatos [0]
+    time.sleep(60)
+    keyboard.press_and_release('ctrl + w')
